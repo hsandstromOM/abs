feadae674680f70c0fc9cd82cef00de46b6ed337
Obviouslee <hosea@obviouslee.com>
2017-01-17 17:30:15 -0500

    more css adjustments

--- components/shared/footer.js
+++ components/shared/footer.js
@@ -166,9 +166,14 @@ function template () {
      h("div#bloc-20.bloc.l-bloc.bgc-white.bloc-tile-3", [
        h("div.container.bloc-md", [
          h("div.row", [
-           h("div.col-sm-12",[
-             h("h6.mg-md.tk-aaux-next",[
+           h("div.col-sm-12",{
+              "style":"text-align:center",
+           },[
+             h("h6.mg-md.tk-aaux-next",{
+               "style":"tex-align:center",
+             },[
                h("a.mg-md.tk-aaux-next",{
+                 "style":"text-align:center",
                  "href":"http://obviouslee.com"
                },"SITE BY OBVIOUSLEE MARKETING")
              ])
