84acbd9603b32793b6ac0192636b2ce8689e0e3a
hsandstromOM <Lakyn4ever>
2017-02-20 22:04:41 -0500

    301 redirects all set

--- browser.js
+++ browser.js
@@ -59,7 +59,59 @@ ng.module('app', [
       accessToken: '2ef82748feb6fd9e7d78f7103794d27612337c3499abc02d7f21c1fb4ee5c627'
     })
 
-
+    // 301 Redirects
+    //Home
+    $urlRouterProvider.when('/pages/page.asp?page_id=282699', '/', 301)
+    //Who We Are
+    $urlRouterProvider.when('/pages/page.asp?page_id=289313', '/', 301)
+    //Team
+    $urlRouterProvider.when('/pages/page.asp?page_id=289315', '/team', 301)
+    //CV PDF
+    $urlRouterProvider.when('/uploads/abs_paul_kennedy_cv_672014.pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/Alan_Campbell_Roof_Consult_cv.pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/Alan_Campbell_Life_safety_cv.pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/abs_jason_gregorie_cv_May_2015.pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/abs_bobby_funcik_cv_2016.pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/abs_john_greenan_cv(1).pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/abs_luis_mariaca_cv(1).pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/Steve_Moore_CV_04152016.pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/abs_edward_polk_cv(1).pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/RESUME_AJS_2_4_2016.pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/abs_joe_shahid_cv(1).pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/Parker_Shields_cv.pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/abs_stephanie_w_borzendowski_cv.pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/abs_lee_capell_cv.pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/RKD_CV_52016.pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/abs_wayne_butler_cv(1).pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/abs_scott_harvey_cv(1).pdf', '/team', 301)
+    $urlRouterProvider.when('/uploads/abs_whitney_okon_cv_2014.pdf', '/team', 301)
+    //In The News
+    $urlRouterProvider.when('/pages/page.asp?page_id=304912', '/', 301)
+    //Services
+    $urlRouterProvider.when('/pages/page.asp?page_id=285535', '/services', 301)
+    //Building Enclosure
+    $urlRouterProvider.when('/pages/page.asp?page_id=289324', '/services', 301)
+    //Structural Engineering
+    $urlRouterProvider.when('/structural', '/services', 301)
+    $urlRouterProvider.when('/pages/page.asp?page_id=290920', '/services', 301)
+    //Civil Engineering
+    $urlRouterProvider.when('/pages/page.asp?page_id=285531', '/services', 301)
+    //Architecture
+    $urlRouterProvider.when('/pages/page.asp?page_id=284943', '/services', 301)
+    //Environmental
+    $urlRouterProvider.when('/pages/page.asp?page_id=285528', '/services', 301)
+    //Training & Contuing Education
+    $urlRouterProvider.when('/pages/page.asp?page_id=285532', '/services', 301)
+    $urlRouterProvider.when('/uploads/2016_Asbestos_Course_Schedule(2).pdf', '/services', 301)
+    $urlRouterProvider.when('/uploads/2016_Lead_Course_Schedule_revised.pdf', '/services', 301)
+    $urlRouterProvider.when('/pages/page.asp?page_id=296287', '/services', 301)
+    $urlRouterProvider.when('/pages/page.asp?page_id=296288', '/services', 301)
+    //Contact
+    $urlRouterProvider.when('/pages/page.asp?page_id=289319', '/contact', 301)
+    //Search
+    $urlRouterProvider.when('/search.asp', '/', 301)
+    //Sitemap
+    $urlRouterProvider.when('/sitemap', '/', 301)
     $urlRouterProvider.otherwise('/')
     $stateProvider
       .state('home', require('./components/home'))
--- server.js
+++ server.js
@@ -74,60 +74,6 @@ router.set('/img/*', ecstatic({ root: __dirname + '/www',  handleError: false}))
 router.set('/browser.js', ecstatic({ root: __dirname + '/www',  handleError: false}))
 router.set('/main.css', ecstatic({ root: __dirname + '/www',  handleError: false}))
 
-// 301 Redirects
-//Home
-app.redirect('/pages/page.asp?page_id=282699', '/', 301)
-//Who We Are
-app.redirect('/pages/page.asp?page_id=289313', '/', 301)
-//Team
-app.redirect('/pages/page.asp?page_id=289315', '/team', 301)
-//CV PDF
-app.redirect('/uploads/abs_paul_kennedy_cv_672014.pdf', '/team', 301)
-app.redirect('/uploads/Alan_Campbell_Roof_Consult_cv.pdf', '/team', 301)
-app.redirect('/uploads/Alan_Campbell_Life_safety_cv.pdf', '/team', 301)
-app.redirect('/uploads/abs_jason_gregorie_cv_May_2015.pdf', '/team', 301)
-app.redirect('/uploads/abs_bobby_funcik_cv_2016.pdf', '/team', 301)
-app.redirect('/uploads/abs_john_greenan_cv(1).pdf', '/team', 301)
-app.redirect('/uploads/abs_luis_mariaca_cv(1).pdf', '/team', 301)
-app.redirect('/uploads/Steve_Moore_CV_04152016.pdf', '/team', 301)
-app.redirect('/uploads/abs_edward_polk_cv(1).pdf', '/team', 301)
-app.redirect('/uploads/RESUME_AJS_2_4_2016.pdf', '/team', 301)
-app.redirect('/uploads/abs_joe_shahid_cv(1).pdf', '/team', 301)
-app.redirect('/uploads/Parker_Shields_cv.pdf', '/team', 301)
-app.redirect('/uploads/abs_stephanie_w_borzendowski_cv.pdf', '/team', 301)
-app.redirect('/uploads/abs_lee_capell_cv.pdf', '/team', 301)
-app.redirect('/uploads/RKD_CV_52016.pdf', '/team', 301)
-app.redirect('/uploads/abs_wayne_butler_cv(1).pdf', '/team', 301)
-app.redirect('/uploads/abs_scott_harvey_cv(1).pdf', '/team', 301)
-app.redirect('/uploads/abs_whitney_okon_cv_2014.pdf', '/team', 301)
-//In The News
-app.redirect('/pages/page.asp?page_id=304912', '/', 301)
-//Services
-app.redirect('/pages/page.asp?page_id=285535', '/services', 301)
-//Building Enclosure
-app.redirect('/pages/page.asp?page_id=289324', '/services', 301)
-//Structural Engineering
-app.redirect('/structural', '/services', 301)
-app.redirect('/pages/page.asp?page_id=290920', '/services', 301)
-//Civil Engineering
-app.redirect('/pages/page.asp?page_id=285531', '/services', 301)
-//Architecture
-app.redirect('/pages/page.asp?page_id=284943', '/services', 301)
-//Environmental
-app.redirect('/pages/page.asp?page_id=285528', '/services', 301)
-//Training & Contuing Education
-app.redirect('/pages/page.asp?page_id=285532', '/services', 301)
-app.redirect('/uploads/2016_Asbestos_Course_Schedule(2).pdf', '/services', 301)
-app.redirect('/uploads/2016_Lead_Course_Schedule_revised.pdf', '/services', 301)
-app.redirect('/pages/page.asp?page_id=296287', '/services', 301)
-app.redirect('/pages/page.asp?page_id=296288', '/services', 301)
-//Contact
-app.redirect('/pages/page.asp?page_id=289319', '/contact', 301)
-//Search
-app.redirect('/search.asp', '/', 301)
-//Sitemap
-// app.redirect('/sitemap', '/', 301)
-
 
 var server = http.createServer(function (req, res) {
   if (cors.apply(req, res)) return res.end()
