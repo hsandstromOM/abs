897944acb0e52f47d88fc8f5a54d566ff455ab48
hsandstromOM <hosea@obviouslee.com>
2017-06-02 10:05:15 -0400

    More speed

--- server.js
+++ server.js
@@ -27,6 +27,24 @@ var mandrillTransport = require('nodemailer-mandrill-transport')
 // load inprocess service
 var ee = require('./services')()
 var compression = require('compression')
+var mcache = require('memory-cache')
+var cache = (duration) => {
+  return (req, res, next) => {
+    let key = '__express__' + req.originalUrl || req.url
+    let cachedBody = mcache.get(key)
+    if (cachedBody) {
+      res.send(cachedBody)
+      return
+    } else {
+      res.sendResponse = res.send
+      res.send = (body) => {
+        mcache.put(key, body, duration * 1000);
+        res.sendResponse(body)
+      }
+      next()
+    }
+  }
+}
 
 app.use(compression())
 
